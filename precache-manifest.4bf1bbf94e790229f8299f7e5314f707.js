57522c61b55317d07cb125b6c47cd19c572314b4
sarva80 <sarvanand801@gmail.com>
2025-05-21 16:21:57 +0530

    Updates

---
+++ precache-manifest.4bf1bbf94e790229f8299f7e5314f707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b995f1e6db44cd43bc5106317fccf8",
+    "url": "/fresh-end/index.html"
+  },
+  {
+    "revision": "d42911d4fa0a241351be",
+    "url": "/fresh-end/static/css/main.52aa204c.chunk.css"
+  },
+  {
+    "revision": "7948d26198f3993d19c0",
+    "url": "/fresh-end/static/js/2.c2c495ad.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/fresh-end/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "d42911d4fa0a241351be",
+    "url": "/fresh-end/static/js/main.694af365.chunk.js"
+  },
+  {
+    "revision": "ca961c99119492a50623",
+    "url": "/fresh-end/static/js/runtime~main.db26cefe.js"
+  }
+]);
